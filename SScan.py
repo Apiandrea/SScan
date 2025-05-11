4f94fa2141b5b81efd66ba62dc0d7860b34a2d4f
Apiandrea <apinadrea@gmail.com>
2025-05-11 18:16:55 +0200

    Created menus and scans. TODO: DDos

--- SScan.py
+++ SScan.py
@@ -47,64 +47,107 @@ def select_device(_IPs):
     
     return _IPs[_device_nmb]
 
-def menu():
+def menu(IP):
     space()
     print_logo()
-    
-    print("""
-    \t[0] > Search another device?
-    \t[1] > Scans?
-    \t[2] > DDos?
-          """)
+    res = 99
+
+    while res < -1 or res > 2:
+        print(f"""
+        Target IP: {IP} 
+        \t[0] > Search another device?
+        \t[1] > Scans?
+        \t[2] > DDos?
+        \t[-1] > Quit.
+        """)
+        res =  int(input("Insert: "))
+
+    return res
+
+def scan_menu(IP): 
+    space()
+    res = 0
+    while res < 1 or res > 2:
+        print(f"""
+        Target IP: {IP} 
+        \t[1] > Default Scan
+        \t[2] > Port Scan 
+        """)
+        res = int(input("Insert: "))
+
+    return res
+
 
 def __main__():
-
 # Controls if the user is with root privileges.
     assert os.geteuid() == 0, Fore.RED + "\n\n[!] You have to run this script as root!\n[i] README.md for help" + Fore.RESET
 
-    print_logo()
+    loop_feed = 0
+    while loop_feed != -1:
+        if loop_feed == 0:
+            print_logo()
 
-    IP = input_IP()
+            IP = input_IP()
 
-    CIDR = -1
-    while CIDR < 0 or CIDR > 32:
-        print(Fore.CYAN + "Insert 0 if you want to scan a single device")
-        CIDR = int(input(Fore.CYAN + "Insert the CIDR number: " + Fore.RESET))
+            CIDR = -1
+            while CIDR < 0 or CIDR > 32:
+                print(Fore.CYAN + "Insert 0 if you want to scan a single device")
+                CIDR = int(input(Fore.CYAN + "Insert the CIDR number: " + Fore.RESET))
 
-    if CIDR == 0:
-        target = f"{IP}"
-    else:
-        target = f"{IP}/{CIDR}"
+            if CIDR == 0:
+                target = f"{IP}"
+            else:
+                target = f"{IP}/{CIDR}"
 
-    space()
+            space()
 
-    _scan_result = Nmap.default_scan(target)
+            _scan_result = Nmap.default_scan(target)
 
-    _gold = []
-    _IPs = []
+            _gold = []
+            _IPs = []
 
-    for s in _scan_result:
-        if "MAC" in s:
-            _gold.append(Fore.GREEN + s + "\n" + Fore.RESET)
-        elif "Nmap scan report" in s:
-            try:
-                _IPs.append(str(len(_IPs)) + ": " + s.split(" ")[5].replace(")", "").replace("(", ""))
-            except Exception as e:
-                print("", end="") 
-            _gold.append(Fore.BLUE + s + Fore.RESET)
+            for s in _scan_result:
+                if "MAC" in s:
+                    _gold.append(Fore.GREEN + s + "\n" + Fore.RESET)
+                elif "Nmap scan report" in s:
+                    try:
+                        _IPs.append(str(len(_IPs)) + ": " + s.split(" ")[5].replace(")", "").replace("(", ""))
+                    except Exception as e:
+                        print("", end="") 
+                    _gold.append(Fore.BLUE + s + Fore.RESET)
 
-    print_scan(_gold)
+            print_scan(_gold)
 
-    space()
+            space()
 
-    if len(_IPs) == 0:
-        print(Fore.RED + "No devices found... Closing..." + Fore.RESET)
-        quit()
+            if len(_IPs) == 0:
+                print(Fore.RED + "No devices found... Closing..." + Fore.RESET)
+                quit()
 
-    if CIDR > 0:
-        IP = select_device(_IPs)
+            if CIDR > 0:
+                IP = select_device(_IPs)
 
-    scanner = Nmap(IP)
+            scanner = Nmap(IP)
+
+        elif loop_feed == 1:
+            scan_feed = scan_menu(IP)
+
+            port_range = 0
+            if scan_feed == 1:
+                space()
+                print(scanner.default_scan(IP))
+            elif scan_feed == 2:
+                while port_range < 1 or port_range > 65535:
+                    port_range = int(input("Insert the port range (1 - 65535)"))
+                space()
+                print(scanner.port_scan(port_range))
+
+        elif loop_feed == 2:
+            print("TODO")
+       
+        loop_feed = menu(IP)
+
+    quit()
 
 
 
